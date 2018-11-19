1a1ec689f1329ed10d7ce9388458703e7acf487c
Philip Graf <git@acanda.ch>
2018-11-19 18:52:50 +0100

    Fix download_data.py

--- download_data.py
+++ download_data.py
@@ -1,4 +1,3 @@
-from tqdm import tqdm
 import urllib.request
 from pathlib import Path
 
@@ -8,6 +7,7 @@ def download(url, file):
         print(f"{file} already exists")
     else:
         print(f"Downloading {file}")
+        Path(file).parent.mkdir(parents=True, exist_ok=True)
         urllib.request.urlretrieve(url, file)
 
 
@@ -18,14 +18,14 @@ print("\nTopic 2")
 download("http://stat-computing.org/dataexpo/2009/2008.csv.bz2", "topic-2/data/2008.csv.bz2")
 
 print("\nTopic 3")
-download("https://github.com/Yorko/mlcourse.ai/blob/master/data/mlbootcamp5_train.csv", "topic-3/data/mlbootcamp5_train.csv")
+download("https://raw.githubusercontent.com/Yorko/mlcourse.ai/master/data/mlbootcamp5_train.csv", "topic-3/data/mlbootcamp5_train.csv")
 
 print("\nTopic 4")
 print("Manually download from Kaggle: https://www.kaggle.com/c/catch-me-if-you-can-intruder-detection-through-webpage-session-tracking2/data")
 
 
 print("\nTopic 5")
-download("https://github.com/Yorko/mlcourse.ai/tree/master/data/credit_scoring_sample.csv", "topic-5/data/credit_scoring_sample.csv")
+download("https://raw.githubusercontent.com/Yorko/mlcourse.ai/master/data/credit_scoring_sample.csv", "topic-5/data/credit_scoring_sample.csv")
 print("Manually download: https://d.pr/f/W0HpZh")
 
 print("\nTopic 6")
